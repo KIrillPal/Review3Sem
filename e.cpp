09e6c085c79910aedad9d2b005175bf8641338fc
KIrillPal <49895546+KIrillPal@users.noreply.github.com>
2022-11-04 11:03:09 +0300

    Create e.cpp

---
+++ e.cpp
@@ -0,0 +1,22 @@
+﻿﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	std::vector<bool> canwin(n + 1);
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j * j <= i; ++j)
+		{
+			if (canwin[i - j] == false) {
+				canwin[i] = true;
+				break;
+			}
+		}
+	}
+	std::cout << (canwin[n] ? "First\n" : "Second\n") << '\n';
+	return 0;
+}
