4d5193c28aafeb6b0117b4939c51c46897d6a2f2
KirillPal <kirpall@ya.ru>
2022-11-05 14:23:30 +0300

    Fix H

--- h.cpp
+++ h.cpp
@@ -1,21 +1,28 @@
-﻿﻿#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 #include <vector>
+#include <climits>
+
+void relaxMinSquares(int i, std::vector<int>& min_squares) {
+	for (int sq = 1; sq * sq <= i; ++sq) {
+		min_squares[i] = std::min(min_squares[i], min_squares[i - sq * sq] + 1);
+	}
+}
+
+int getMinSquareSum(int n) 
+{
+	std::vector<int> min_squares(n + 1, INT_MAX);
+	min_squares[0] = 0;
+	for (int i = 1; i <= n; ++i) {
+		relaxMinSquares(i, min_squares);
+	}
+	return min_squares[n];
+}
 
 int main()
 {
 	int n;
 	std::cin >> n;
-
-	const int MAXCOST = 1000001;
-	std::vector<int> min_squares(n + 1, MAXCOST);
-	min_squares[0] = 0;
-
-	for (int i = 1; i <= n; ++i) {
-		for (int sq = 1; sq * sq <= i; ++sq) {
-			min_squares[i] = std::min(min_squares[i], min_squares[i - sq * sq] + 1);
-		}
-	}
-	std::cout << min_squares[n] << '\n';
+	std::cout << getMinSquareSum(n) << '\n';
 	return 0;
 }
