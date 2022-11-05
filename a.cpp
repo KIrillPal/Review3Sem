79862f761ecdb8cdca58ede77cbe30054380d61e
KirillPal <kirpall@ya.ru>
2022-11-05 12:19:42 +0300

    Fix A

--- a.cpp
+++ a.cpp
@@ -2,19 +2,21 @@
 #include <algorithm>
 #include <vector>
 
-using namespace std;
-
-int main()
-{
-	int n, m;
-	cin >> n >> m;
-	vector<int> cnts(n + 1);
+int grasshopper(int n, int m) {
+	std::vector<int> cnts(n + 1);
 	cnts[0] = 1;
 	for (int i = 1; i <= n; ++i) {
 		for (int j = i - 1; j >= 0 && i - j <= m; --j) {
 			cnts[i] += cnts[j];
 		}
 	}
-	cout << cnts[n] << '\n';
+	return cnts[n];
+}
+
+int main()
+{
+	int n, m;
+	std::cin >> n >> m;
+	std::cout << grasshopper(n, m) << '\n';
 	return 0;
 }
