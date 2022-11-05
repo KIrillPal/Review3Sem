dd4bf52fe1d7f56480799d3006c6e35a0d796458
KirillPal <kirpall@ya.ru>
2022-11-05 13:27:38 +0300

    Fix F

--- f.cpp
+++ f.cpp
@@ -2,9 +2,22 @@
 #include <algorithm>
 #include <vector>
 
+#define MAXN 300
+
+bool moveLine(int x, int y, int dx, int dy, int n, int m, bool canwin[MAXN][MAXN]) {
+	int i = 1;
+	while (x + i * dx < n && y + i * dy < m) {
+		if (canwin[x + i * dx][y + i * dy] == false) {
+			canwin[x][y] = true;
+			return true;
+		}
+		++i;
+	}	
+	return false;
+}
+
 int main()
 {
-	const int MAXN = 300;
 
 	int n, m;
 	std::cin >> n >> m;
@@ -18,26 +31,14 @@ int main()
 				continue;
 			}
 
-			for (int k = j + 1; k < m; ++k) {
-				if (canwin[i][k] == false) {
-					canwin[i][j] = true;
-					break;
-				}
-			}
+			if (moveLine(i, j, 0, 1, n, m, canwin))
+				continue;
 
-			for (int k = i + 1; k < n; ++k) {
-				if (canwin[k][j] == false) {
-					canwin[i][j] = true;
-					break;
-				}
-			}
+			if (moveLine(i, j, 1, 0, n, m, canwin))
+				continue;
 
-			for (int k = 1; i + k < n && j + k < m; ++k) {
-				if (canwin[i + k][j + k] == false) {
-					canwin[i][j] = true;
-					break;
-				}
-			}
+			if (moveLine(i, j, 1, 1, n, m, canwin))
+				continue;
 		}
 	}
 	std::cout << (canwin[0][0] ? "F\n" : "S\n") << '\n';
