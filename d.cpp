f7a5b3376b90069653f9693be7221a627c9570f5
KirillPal <kirpall@ya.ru>
2022-11-05 12:57:30 +0300

    Fix D

--- d.cpp
+++ d.cpp
@@ -3,18 +3,28 @@
 #include <string>
 #include <vector>
 
-int main()
-{
-	std::string a, b;
-	std::cin >> a >> b;
+void relaxSeg(int j, int& best_li, int& best_ri, int& best_di, const std::string& b) {
+	int li = j - best_di;
+	for (int k = 0; k < best_di; ++k) {
+		if (b[best_li + k] > b[li + k]) {
+			best_li = li;
+			best_ri = j;
+			break;
+		}
+		else if (b[best_li + k] < b[li + k])
+			break;
+	}
+}
+
+std::string solve(const std::string& a, const std::string& b) {
 	int a_size = a.size();
 	int b_size = b.size();
 	int best_li = 0, best_ri = 0, best_di = 0;
 
 	std::vector<int> old_dp(b_size + 1);
 	std::vector<int> new_dp(b_size + 1);
+	
 	for (int i = 1; i <= a_size; ++i) {
-
 		for (int j = 1; j <= b_size; ++j)
 		{
 			if (a[i - 1] == b[j - 1])
@@ -26,16 +36,7 @@ int main()
 					best_ri = j;
 				}
 				else if (best_di == new_dp[j]) {
-					int li = j - best_di;
-					for (int k = 0; k < best_di; ++k) {
-						if (b[best_li + k] > b[li + k]) {
-							best_li = li;
-							best_ri = j;
-							break;
-						}
-						else if (b[best_li + k] < b[li + k])
-							break;
-					}
+					relaxSeg(j, best_li, best_ri, best_di, b);					
 				}
 			}
 			else new_dp[j] = 0;
@@ -43,6 +44,13 @@ int main()
 		old_dp.swap(new_dp);
 	}
 
-	std::cout << b.substr(best_li, best_di) << '\n';
+	return b.substr(best_li, best_di);
+}
+
+int main()
+{
+	std::string a, b;
+	std::cin >> a >> b;
+	std::cout << solve(a, b) << '\n';
 	return 0;
 }
