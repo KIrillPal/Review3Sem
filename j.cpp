739c12018d274d6b8b9433b91348c32d5b255d5a
KirillPal <kirpall@ya.ru>
2022-11-06 11:13:28 +0300

    Fix J

--- j.cpp
+++ j.cpp
@@ -1,19 +1,16 @@
-﻿﻿#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 #include <bitset>
 #include <vector>
 
 typedef unsigned long long ull;
 
-int main()
-{
-	int n;
-	scanf("%d", &n);
-	std::vector<int> ones_row(n), ones_col(n);
+size_t solve(size_t n) {
+	std::vector<size_t> ones_row(n), ones_col(n);
 
-	std::vector<int> old_sq(n);
-	std::vector<int> new_sq(n);
-	int max_size = 0;
+	std::vector<size_t> old_sq(n);
+	std::vector<size_t> new_sq(n);
+	size_t max_size = 0;
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < n; ++j)
@@ -30,11 +27,17 @@ int main()
 			new_sq[j] = (j > 0 ? old_sq[j - 1] + 1 : 1);
 			new_sq[j] = std::min(new_sq[j], ones_row[i]);
 			new_sq[j] = std::min(new_sq[j], ones_col[j]);
-			max_size = std::max(max_size, new_sq[j]);
+			max_size  = std::max(max_size, new_sq[j]);
 		}
 		new_sq.swap(old_sq);
 	}
+	return max_size;
+}
 
-	printf("%d\n", max_size);
+int main()
+{
+	size_t n;
+	scanf("%zu", &n);
+	printf("%zu\n", solve(n));
 	return 0;
 }
