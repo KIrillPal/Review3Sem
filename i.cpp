d19a5445bb4d2e4da7e3e9e9a3ba23c90ba48a7a
KirillPal <kirpall@ya.ru>
2022-11-06 10:56:28 +0300

    Fix I

--- i.cpp
+++ i.cpp
@@ -5,12 +5,17 @@
 
 typedef unsigned long long ull;
 
-int main()
-{
-	std::string str;
-	std::cin >> str;
-	int n = str.size();
+void relaxCombs(std::vector<std::vector<ull> > &pal_combs, int l, int r, std::string& str) {
+	if (str[l] == str[r - 1])
+		pal_combs[l][r] += pal_combs[l + 1][r - 1];
 
+	pal_combs[l][r] += pal_combs[l][r - 1];
+	pal_combs[l][r] += pal_combs[l + 1][r];
+	pal_combs[l][r] -= pal_combs[l + 1][r - 1];
+}
+
+ull findAnswer(int n, std::string& str) 
+{
 	std::vector<std::vector<ull> > pal_combs(n + 1, std::vector<ull>(n + 1));
 	for (int i = 0; i < n; ++i) {
 		pal_combs[i][i] = 1;
@@ -21,17 +26,19 @@ int main()
 		for (int l = 0; l <= n - length; ++l)
 		{
 			int r = l + length;
-
-			if (str[l] == str[r - 1])
-				pal_combs[l][r] += pal_combs[l + 1][r - 1];
-
-			pal_combs[l][r] += pal_combs[l][r - 1];
-			pal_combs[l][r] += pal_combs[l + 1][r];
-			pal_combs[l][r] -= pal_combs[l + 1][r - 1];
-
+			relaxCombs(pal_combs, l, r, str);
 		}
 	}
 
-	std::cout << pal_combs[0][n] << '\n';
+	return pal_combs[0][n];
+}
+
+int main()
+{
+	std::string str;
+	std::cin >> str;
+	int n = str.size();
+
+	std::cout << findAnswer(n, str) << '\n';
 	return 0;
 }
